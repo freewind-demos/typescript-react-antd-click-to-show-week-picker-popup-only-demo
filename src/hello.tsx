b3fc16472b0aca66cd9076258e8960a0d45d3942
Freewind <nowindlee@gmail.com>
2023-01-21 07:54:18 +0800

    Simplified again, thanks @saiqi!

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,24 +1,36 @@
-import React, {useRef, useState} from 'react'
+import React from 'react'
 import {Button, DatePicker} from 'antd';
-import {useClickAway} from "ahooks";
 
 export default function Hello() {
-    const [show, setShow] = useState(false);
-
-    const ref = useRef<HTMLButtonElement>(null);
-    useClickAway(() => setShow(false), ref);
-
     return <div>
         <h1>Click to show simplified week picker</h1>
         <DatePicker.WeekPicker
             bordered={false}
-            open={show}
+            // open={show} // Don't control the state
+            allowClear={false}
             style={{padding: 0}}
-            inputRender={() => <Button ref={ref}
-                                       onClick={() => setShow(v => !v)}>Hello, click to show the week picker</Button>}
-            inputReadOnly={true}
+            inputRender={({id, tabIndex, disabled, autoFocus, title, onMouseDown, onKeyDown, onFocus, onBlur}) =>
+                <Button id={id}
+                        tabIndex={tabIndex}
+                        disabled={disabled}
+                        autoFocus={autoFocus}
+                        title={title}
+                        onMouseDown={onMouseDown}
+                        onKeyDown={onKeyDown}
+                        onFocus={onFocus}
+                        onBlur={onBlur}
+                >Hello, click to show the week
+                    picker</Button>}
             suffixIcon={undefined}
-        ></DatePicker.WeekPicker>
+        />
+        <DatePicker.WeekPicker
+            bordered={false}
+            // open={show} // Don't control the state
+            allowClear={false}
+            style={{padding: 0}}
+            inputRender={(props) => <Button {...props as any}>Dirty and lazy way</Button>}
+            suffixIcon={undefined}
+        />
         <div>other text</div>
     </div>
 };
