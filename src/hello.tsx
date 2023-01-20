c198c9b458680eb79be04c75d7a45d0ad78ea552
Freewind <nowindlee@gmail.com>
2023-01-20 22:10:14 +0800

    use useClickAway to fix

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,15 +1,21 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import {Button, DatePicker} from 'antd';
+import {useClickAway} from "ahooks";
 
 export default function Hello() {
     const [show, setShow] = useState(false);
+
+    const ref = useRef<HTMLButtonElement>(null);
+    useClickAway(() => setShow(false), ref);
+
     return <div>
         <h1>Click to show simplified week picker</h1>
         <DatePicker.WeekPicker
             bordered={false}
             open={show}
             style={{padding: 0}}
-            inputRender={() => <Button onClick={() => setShow(v => !v)}>Hello, click to show the week picker</Button>}
+            inputRender={() => <Button ref={ref}
+                                       onClick={() => setShow(v => !v)}>Hello, click to show the week picker</Button>}
             inputReadOnly={true}
             suffixIcon={undefined}
         ></DatePicker.WeekPicker>
